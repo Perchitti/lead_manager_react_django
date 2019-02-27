30bb8332776673745d915baa3fdc389b0574eff3
Perchitti <LaurenPerchitti@gmail.com>
2019-02-26 20:53:45 -0500

    ADD_LEADS added
    form working and adding

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { getLeads } from '../../actions/leads'
+import { getLeads, deleteLead } from '../../actions/leads'
 
 export class Leads extends Component {
     static propTypes = {
-        leads: PropTypes.array.isRequired
+        leads: PropTypes.array.isRequired,
+        getLeads: PropTypes.func.isRequired,
+        deleteLead: PropTypes.func.isRequired
     }
 
     componentDidMount() {
@@ -29,10 +31,14 @@ export class Leads extends Component {
                 <tbody>
                     { this.props.leads.map(lead => (
                         <tr key={lead.id}>
+                            <td>{lead.id}</td>
                             <td>{lead.name}</td>
                             <td>{lead.email}</td>
                             <td>{lead.message}</td>
-                            <td><button className="btn btn-danger btn-sm">Delete</button></td>
+                            <td>
+                                <button onClick={this.props.deleteLead.bind(this, lead.id)} className="btn btn-danger btn-sm"> 
+                            {" "}
+                            Delete</button></td>
                         </tr>
                     ))}
                 </tbody>
@@ -46,4 +52,4 @@ const mapStateToProps = state => ({
     leads: state.leads.leads
 })
 
-export default connect(mapStateToProps, { getLeads })(Leads)
+export default connect(mapStateToProps, { getLeads, deleteLead })(Leads)
